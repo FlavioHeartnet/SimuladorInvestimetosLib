1945700eeb94368e02144313fb7702cc5edf5790
Flavio Nogueira <flavionogueirabarros@gmail.com>
2025-05-22 17:32:30 -0300

    formating names

--- SimuladorInvestimentos/Program.cs
+++ SimuladorInvestimentos/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using NMoneys;
 using SimuladorInvestimentos.Core;
 using SimuladorInvestimentos.Core.Entity;
 
 var sim =  Simulation.CreateSimulation(Decimal.Parse("500.00") , Decimal.Parse("14"), 11, 0.7, false, 15 );
 decimal montanteFinal = new MontanteFinal(sim).Calcular();
-Console.WriteLine("Montante: "  + montanteFinal);
+var currency = new Money(montanteFinal, CurrencyIsoCode.BRL);
+Console.WriteLine("Montante: "  + currency);
